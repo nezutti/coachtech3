63d9bf3e139e8ae507ee91700103c32e9c281f9a
久保祐輔 <bokukuboyu@icloud.com>
2022-05-08 16:16:32 +0900

    modal

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-const tabs = document.getElementsByClassName("tab-menu__item");
-for (let i = 0; i < tabs.length; i++){
-  tabs[i].addEventListener("click", tabSwitch);
-
-}//要素のインデックス番号がtabs.length(この場合[i]が”0~2")の範囲の中である場合この処理を繰り返す
-
-function tabSwitch() {
-  document.getElementsByClassName("active")[0].classList.remove("active");//デフォルトで設定してあるタブ1からactiveクラスを消す
-  this.classList.add("active");//acitveクラスを追加する
-  document.getElementsByClassName("show")[0].classList.remove("show");//デフォルトで設定してある、コンテンツ１からshowクラスを消す
-  const arrayTabs = Array.prototype.slice.call(tabs);  //tabsの要素を入れた配列を作る
-
-  const index = arrayTabs.indexOf(this);//arraytabsから要素のインデックス番号を取り出す
-   document.getElementsByClassName('tab-content__item')[index].classList.add('show');//取り出した要素をshowクラスを見えるようにすることによって表示させる
-
- 
-};
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
+const modal = document.getElementById('modal');
+openBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+})
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+})
+window.addEventListener('click', (e) => {
+  if (!e.target.closest('.modal__content-inner') && e.target.id !== "openBtn") {
+    modal.style.display = 'none';
+    console.log(e);
+  }
+});
